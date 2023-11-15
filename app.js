da8eaf53cb3e329ec7d7e0691dfd6a8b2a3fff01
Glinarez97 <148172969+Glinarez97@users.noreply.github.com>
2023-11-14 21:22:52 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+function runApp(){
+    let s=prompt("Enter f or c");
+    let t=Number(prompt("Enter temp:"));
+    let e=Number(prompt("Enter end temp:"));
+    convertTemperature(s,t);
+}
+function convertTemperature(scale,temp){
+    let result;
+    if(scale == "f"){
+        result= (temp - 32) * 5/9;
+        console.log(result);
+        
+    }else if(scale=="c"){
+        result = (temp * 9/5) + 32;
+        console.log(result);
+    }else{
+        console.log("Incorrect scale");
+    }
+}
